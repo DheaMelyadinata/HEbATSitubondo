9a3d5e64ff808bccf1947780748ef7843530b1cd
Dhea Melyadinata <dmelyadinata@gmail.com>
2020-03-07 13:40:44 +0700

    Membuat activity layout dan java kontak kami

---
+++ app/src/main/java/com/example/hebatsitubondo/KontakKami.java
@@ -0,0 +1,13 @@
+package com.example.hebatsitubondo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KontakKami extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kontak_kami);
+    }
+}
