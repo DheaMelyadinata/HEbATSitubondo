07448b12ec26e030f95bb70f721db105d148d165
Dhea Melyadinata <dmelyadinata@gmail.com>
2020-03-19 20:42:27 +0700

    update intent layout agenda kegiatan

--- app/src/main/java/com/example/hebatsitubondo/Dashboard.java
+++ app/src/main/java/com/example/hebatsitubondo/Dashboard.java
@@ -1,8 +1,11 @@
 package com.example.hebatsitubondo;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -12,6 +15,9 @@ import com.synnapps.carouselview.ImageClickListener;
 import com.synnapps.carouselview.ImageListener;
 
 public class Dashboard extends AppCompatActivity {
+
+//    private BottomNavigationView bottomNavigation;
+
     private int[] mImage = new int[]{
          R.drawable.kegiatan1, R.drawable.kegiatan2, R.drawable.kegiatan3
     };
@@ -24,6 +30,30 @@ public class Dashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+//        bottomNavigation = findViewById(R.id.bottom_navigation);
+//        bottomNavigation.inflateMenu(R.menu.bottom_navigation);
+//
+//        //Memberikan listener saat menu item di bottom navigation diklik
+//        bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                int id = item.getItemId();
+//                switch (id) {
+//                    case R.id.nav_home:
+//                        Toast.makeText(Dashboard.this, "Anda sedang berada di beranda", Toast.LENGTH_SHORT).show();
+//                        break;
+//                    case R.id.nav_kalender:
+//                        Toast.makeText(Dashboard.this, "Jadwal clicked", Toast.LENGTH_SHORT).show();
+//                        break;
+//                    case R.id.nav_akun:
+//                        Intent intent0 = new Intent(Dashboard.this, ProfilUser.class);
+//                        startActivity(intent0);
+//                        break;
+//                }
+//                return true;
+//            }
+//        });
 
         CarouselView carouselView = findViewById(R.id.carousel);
         carouselView.setPageCount(mImage.length);
@@ -41,11 +71,6 @@ public class Dashboard extends AppCompatActivity {
         });
     }
 
-    public void Kontak(View view) {
-        Intent intent = new Intent(Dashboard.this, KontakKami.class);
-        startActivity(intent);
-    }
-
     public void Portofolio(View view) {
         Intent intent = new Intent(Dashboard.this, Portofolio.class);
         startActivity(intent);
@@ -53,6 +78,15 @@ public class Dashboard extends AppCompatActivity {
 
     public void Akegiatan(View view) {
         Intent intent = new Intent(Dashboard.this, JurnalKegiatan.class);
+        startActivity(intent);
+    }
+
+    public void AgendaKegiatan(View view) {
+        Intent intent = new Intent(Dashboard.this, AgendaKegiatan.class);
+        startActivity(intent);
+    }
+    public void Kontak(View view) {
+        Intent intent = new Intent(Dashboard.this, KontakKami.class);
         startActivity(intent);
     }
 }
